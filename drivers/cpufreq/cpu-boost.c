c88453e02546aaa247643f76d7f8767e9161e661
Francisco Franco <franciscofranco.1990@gmail.com>
2017-03-22 08:36:21 +0000

    cpufreq: cpu-boost: don't boost big cluster on input touch unless it has at least 1 task running on any of its cores to save power
    Signed-off-by: The-DarkBeast <pranaynarang@gmail.com>
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>#
    (use "git push" to publish your local commits)
    
    Signed-off-by: ShreyanshLodha <slodha96@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -25,6 +25,11 @@
 #include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/time.h>
+<<<<<<< HEAD
+=======
+
+#include "../../kernel/sched/sched.h"
+>>>>>>> 441cc3723dc... cpufreq: cpu-boost: don't boost big cluster on input touch
 
 struct cpu_sync {
 	struct task_struct *thread;
@@ -38,6 +43,7 @@ struct cpu_sync {
 	unsigned int input_boost_min;
 	unsigned int task_load;
 	unsigned int input_boost_freq;
+	unsigned int nr_running;
 };
 
 static DEFINE_PER_CPU(struct cpu_sync, sync_info);
@@ -75,6 +81,11 @@ static bool sched_boost_active;
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
 #define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
+<<<<<<< HEAD
+=======
+
+static unsigned int cnt_nr_running;
+>>>>>>> 441cc3723dc... cpufreq: cpu-boost: don't boost big cluster on input touch
 
 static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 {
@@ -171,6 +182,7 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 
 		min = max(b_min, ib_min);
 
+<<<<<<< HEAD
 		pr_debug("CPU%u policy min before boost: %u kHz\n",
 			 cpu, policy->min);
 		pr_debug("CPU%u boost min: %u kHz\n", cpu, min);
@@ -181,6 +193,21 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 			 cpu, policy->min);
 		break;
 
+=======
+		if (cpu == 4 && min > 0 && cnt_nr_running == 0)
+                        break;
+
+		pr_debug("CPU%u policy min before boost: %u kHz\n",
+			 cpu, policy->min);
+		pr_debug("CPU%u boost min: %u kHz\n", cpu, min);
+
+		cpufreq_verify_within_limits(policy, min, UINT_MAX);
+
+		pr_debug("CPU%u policy min after boost: %u kHz\n",
+			 cpu, policy->min);
+		break;
+
+>>>>>>> 441cc3723dc... cpufreq: cpu-boost: don't boost big cluster on input touch
 	case CPUFREQ_START:
 		set_cpus_allowed(s->thread, *cpumask_of(cpu));
 		break;
@@ -212,7 +239,8 @@ static void update_policy_online(void)
 	get_online_cpus();
 	for_each_online_cpu(i) {
 		pr_debug("Updating policy for CPU%d\n", i);
-		cpufreq_update_policy(i);
+		if (i == 0 || i == 4)
+			cpufreq_update_policy(i);
 	}
 	put_online_cpus();
 }
@@ -228,6 +256,8 @@ static void do_input_boost_rem(struct work_struct *work)
 		i_sync_info = &per_cpu(sync_info, i);
 		i_sync_info->input_boost_min = 0;
 	}
+
+	cnt_nr_running = 0;
 
 	/* Update policies for all online CPUs */
 	update_policy_online();
@@ -368,6 +398,9 @@ static void do_input_boost(struct work_struct *work)
 	for_each_possible_cpu(i) {
 		i_sync_info = &per_cpu(sync_info, i);
 		i_sync_info->input_boost_min = i_sync_info->input_boost_freq;
+
+		if (i >= 4)
+			cnt_nr_running += cpu_rq(i)->nr_running;
 	}
 
 	/* Update policies for all online CPUs */
@@ -395,7 +428,11 @@ static void cpuboost_input_event(struct input_handle *handle,
 		return;
 
 	now = ktime_to_us(ktime_get());
+<<<<<<< HEAD
 	if (now - last_input_time < MIN_INPUT_INTERVAL)
+=======
+	if (now - last_input_time < (input_boost_ms * USEC_PER_MSEC))
+>>>>>>> 441cc3723dc... cpufreq: cpu-boost: don't boost big cluster on input touch
 		return;
 
 	if (work_pending(&input_boost_work))
