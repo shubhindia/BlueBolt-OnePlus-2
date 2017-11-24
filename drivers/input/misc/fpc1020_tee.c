3f509a4fbe12f207f6b16779125dfcf32e4183c6
Sultanxda <sultanxda@gmail.com>
2017-11-23 20:10:22 -0800

    Revert "misc: fpc1020: Increase fingerprintd nice value when screen is off"
    Eh, let's just give fingerprintd the highest priority.

--- drivers/input/misc/fpc1020_tee.c
+++ drivers/input/misc/fpc1020_tee.c
@@ -994,13 +994,9 @@ static void fpc1020_suspend_resume(struct work_struct *work)
 	if (fpc1020->screen_state) {
 		set_fpc_irq(fpc1020, true);
 		set_fingerprintd_nice(0);
-	} else
-		/*
-		 * Elevate fingerprintd priority when screen is off to ensure
-		 * the fingerprint sensor is responsive and that the haptic
-		 * response on successful verification always fires.
-		 */
-		set_fingerprintd_nice(-1);
+	} else {
+		set_fingerprintd_nice(-20);
+	}
 
 	sysfs_notify(&fpc1020->dev->kobj, NULL,
 				dev_attr_screen_state.attr.name);
