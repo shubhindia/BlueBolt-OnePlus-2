ada4b5fd53d20f87e1e5ca7fe8efac5c5861e5da
Shreyansh Lodha <slodha96@gmail.com>
2018-02-05 09:31:55 +0530

    Kanged some files from franco's kernel zip
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- AnyKernel2/ramdisk/init.supolicy.sh
+++ AnyKernel2/ramdisk/init.supolicy.sh
@@ -5,6 +5,7 @@ SULIBS="/su/lib:/system/lib64:/system/lib"
 for SUPOLICY in `which supolicy sepolicy-inject`;
 do
 	LD_LIBRARY_PATH=$SULIBS $SUPOLICY --live \
-	"allow untrusted_app sysfs file { read write getattr open }" \
-	"allow untrusted_app sysfs:file { read write getattr open }"
+        "allow mediaserver mediaserver_tmpfs file { read write execute }" \
+	    "allow untrusted_app sysfs file { read write getattr open }" \
+        "allow untrusted_app sysfs dir { read write getattr open }"
 done
