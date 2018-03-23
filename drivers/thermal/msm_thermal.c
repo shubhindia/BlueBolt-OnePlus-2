15df4f0e262301c9206b409b8293496434f277c0
flar2 <asegaert@gmail.com>
2018-03-24 00:46:07 +0530

    thermal: disable thermal-engine hotplug and poll instead
    Also add sysfs node for temp threshold.
    Will probably change this around when I have more time.
    
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -80,6 +80,13 @@
 		_val |= 2;				\
 } while (0)
 
+//custom thermal
+#define DEF_TEMP_THRESHOLD 46
+#define HOTPLUG_SENSOR_ID 18
+#define HOTPLUG_HYSTERESIS 2
+unsigned int temp_threshold = DEF_TEMP_THRESHOLD;
+module_param(temp_threshold, uint, 0644);
+
 static struct msm_thermal_data msm_thermal_info;
 static struct delayed_work check_temp_work;
 static bool core_control_enabled;
@@ -2410,7 +2417,7 @@ static void __ref do_core_control(long temp)
 
 	mutex_lock(&core_control_mutex);
 	if (msm_thermal_info.core_control_mask &&
-		temp >= msm_thermal_info.core_limit_temp_degC) {
+		temp >= temp_threshold) {
 		for (i = num_possible_cpus(); i > 0; i--) {
 			if (!(msm_thermal_info.core_control_mask & BIT(i)))
 				continue;
@@ -2431,8 +2438,7 @@ static void __ref do_core_control(long temp)
 			break;
 		}
 	} else if (msm_thermal_info.core_control_mask && cpus_offlined &&
-		temp <= (msm_thermal_info.core_limit_temp_degC -
-			msm_thermal_info.core_temp_hysteresis_degC)) {
+		temp <= (temp_threshold - HOTPLUG_HYSTERESIS)) {
 		for (i = 0; i < num_possible_cpus(); i++) {
 			if (!(cpus_offlined & BIT(i)))
 				continue;
@@ -2968,6 +2974,18 @@ static void check_temp(struct work_struct *work)
 		return;
 
 	do_therm_reset();
+	
+	if (!polling_enabled) {
+		ret = therm_get_temp(HOTPLUG_SENSOR_ID, THERM_ZONE_ID, &temp);
+		if (ret) {
+			pr_err("Unable to read sensor:%d. err:%d\n",
+				HOTPLUG_SENSOR_ID, ret);
+			goto reschedule;
+		}
+		do_core_control(temp);
+
+		goto reschedule;
+	}
 
 	ret = therm_get_temp(msm_thermal_info.sensor_id, THERM_TSENS_ID, &temp);
 	if (ret) {
@@ -2994,7 +3012,7 @@ static void check_temp(struct work_struct *work)
 	do_freq_control(temp);
 
 reschedule:
-	if (polling_enabled)
+	/* if (polling_enabled) */
 		queue_delayed_work(system_power_efficient_wq,
 			&check_temp_work,
 			msecs_to_jiffies(msm_thermal_info.poll_ms));
@@ -4164,7 +4182,7 @@ cx_node_fail:
 cx_node_exit:
 	return ret;
 }
-
+#if 0
 /*
  * We will reset the cpu frequencies limits here. The core online/offline
  * status will be carried over to the process stopping the msm_thermal, as
@@ -4191,6 +4209,7 @@ static void __ref disable_msm_thermal(void)
 	update_cluster_freq();
 	put_online_cpus();
 }
+#endif
 
 static void interrupt_mode_init(void)
 {
@@ -4201,7 +4220,7 @@ static void interrupt_mode_init(void)
 	if (polling_enabled) {
 		pr_info("Interrupt mode init\n");
 		polling_enabled = 0;
-		disable_msm_thermal();
+		/* disable_msm_thermal(); */
 		hotplug_init();
 		freq_mitigation_init();
 		thermal_monitor_init();
@@ -4238,8 +4257,6 @@ MODULE_PARM_DESC(enabled, "enforce thermal limit on cpu");
 module_param_named(poll_ms, msm_thermal_info.poll_ms, uint, 0664);
 
 /* Temp Threshold */
-module_param_named(temp_threshold, msm_thermal_info.limit_temp_degC,
-			int, 0664);
 module_param_named(core_limit_temp_degC, msm_thermal_info.core_limit_temp_degC,
 		   uint, 0644);
 module_param_named(hotplug_temp_degC, msm_thermal_info.hotplug_temp_degC,
@@ -4333,6 +4350,9 @@ static ssize_t __ref store_cpus_offlined(struct kobject *kobj,
 		pr_err("Invalid input %s. err:%d\n", buf, ret);
 		goto done_cc;
 	}
+	
+	/* return early */
+	goto done_cc;
 
 	if (polling_enabled) {
 		pr_err("Ignoring request; polling thread is enabled.\n");
